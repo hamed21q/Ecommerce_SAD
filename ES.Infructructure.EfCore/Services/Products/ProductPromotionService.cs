9a92b51564d57b61d62ef49a846681662b866245
hamed21q <hasanihamed220@gmail.com>
2022-12-31 01:42:08 -0800

    idont know

--- ES.Infructructure.EfCore/Services/Products/ProductPromotionService.cs
+++ ES.Infructructure.EfCore/Services/Products/ProductPromotionService.cs
@@ -1,11 +1,5 @@
 ﻿using ES.Domain.Entities.Products.Promotion;
 using ES.Infructructure.EfCore.Base;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ES.Infructructure.EfCore.Services.Products
 {
