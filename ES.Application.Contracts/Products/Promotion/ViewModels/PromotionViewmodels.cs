97d3ffe53923e5208d9365c176d17c9898aa58d9
Sara <100788541+sarahmhm1381@users.noreply.github.com>
2023-01-01 21:56:55 +0330

    promotion Application contracts implemented

---
+++ ES.Application.Contracts/Products/Promotion/ViewModels/PromotionViewmodels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES.Application.Contracts.Products.Promotion.ViewModels
+{
+    public class PromotionViewmodels
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+    }
+}
